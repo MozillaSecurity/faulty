9d441dce25acf52b88652ad34e887ba8f8a82773
Christoph Diehl <cdiehl@mozilla.com>
2016-06-01 00:21:57 +0200

    Use patch URL from Faulty repository

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,10 @@
 
 # Download Faulty
-wget https://gist.githubusercontent.com/posidron/802f519bd54a05cc50bb51fbc0563448/raw/451062c31537635622b9ba14615e608718da256e/faulty.v14.diff
+wget https://raw.githubusercontent.com/MozillaSecurity/faulty/master/faulty.v14.diff
 
 # Download mozilla build configs
 git clone --depth=1 https://github.com/posidron/mozilla-build-configs configs
+
 
 # Apply Faulty to mozilla-inbound
 git apply --ignore-space-change --ignore-whitespace faulty.v14.diff
